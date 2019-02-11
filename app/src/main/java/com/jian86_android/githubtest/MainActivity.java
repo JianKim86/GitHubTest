9beb8b553280320742e3da5db5760063f7ca7424
JianKim <43667417+JianKim86@users.noreply.github.com>
2019-02-11 15:58:10 +0900

    update

--- app/src/main/java/com/jian86_android/githubtest/MainActivity.java
+++ app/src/main/java/com/jian86_android/githubtest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("changeTitle");
+        //깃에서 받기
     }
 }
